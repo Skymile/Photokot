1eaea883642d200845f579e821e3efc390aed449
Patryk Milewski <mpatryk97@gmail.com>
2018-04-16 18:31:13 +0200

    System.Drawing

--- Models/Picture.cs
+++ Models/Picture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Models
 {
 	public class Picture
 	{
+		//https://github.com/Skymile/Photokot
+
+		public Picture() { }
+
+		private Bitmap _Bitmap;
 	}
 }
