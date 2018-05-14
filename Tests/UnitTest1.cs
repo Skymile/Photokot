8878cf075a94f1aafa1cde244d64a51623f9fae7
Patryk Milewski <mpatryk97@gmail.com>
2018-05-14 19:20:03 +0200

    Equality testing, differences

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -13,6 +13,15 @@ namespace Tests
 			Models.Picture pictureTwo = new Models.Picture("apple.png");
 			Models.Picture pictureThree = new Models.Picture("apple2.png");
 
+			Assert.IsTrue(pictureOne.GetDifferences(pictureTwo).Count == 0);
+
+			var points = pictureOne.GetDifferences(pictureThree);
+
+			Assert.IsTrue(points.Count == 2);
+
+			Console.WriteLine(points[0].ToString());
+			Console.WriteLine(points[1].ToString());
+
 			Assert.IsTrue(pictureOne == pictureTwo);
 		}
 	}
